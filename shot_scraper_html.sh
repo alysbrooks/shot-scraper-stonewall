47bb7e8fcd6d27213112acc7c3303416c92fd0a2
Alys Brooks <alysbrooks@fastmail.com>
2025-02-15 16:33:08 -0600

    Correct HTML scraping commands.

--- shot_scraper_html.sh
+++ shot_scraper_html.sh
@@ -6,64 +6,64 @@ shot-scraper html https://www.nps.gov/articles/pride-guide.htm -o html/articles/
 shot-scraper html https://www.nps.gov/subjects/tellingallamericansstories/lgbtqresources.htm -o html/subjects/tellingallamericansstories/lgbtqresources.htm
 shot-scraper html https://www.nps.gov/ston/learn/education/new-index.htm -o html/ston/learn/education/new-index.htm
 shot-scraper html http://www.nps.gov/ston/learn/historyculture/recommended-reading-list.htm -o html/ston/learn/historyculture/recommended-reading-list.htm
-shot-scraper html https://www.nps.gov/ston/assemblymessage6-5-20.htm /ston/assemblymessage6-5-20.htm
-shot-scraper html https://www.nps.gov/ston/contacts.htm /ston/contacts.htm
-shot-scraper html https://www.nps.gov/ston/faqs.htm /ston/faqs.htm
-shot-scraper html https://www.nps.gov/ston/foundation-document.htm /ston/foundation-document.htm
-shot-scraper html https://www.nps.gov/ston/getinvolved/2016.htm /ston/getinvolved/2016.htm
-shot-scraper html https://www.nps.gov/ston/getinvolved/2021-year-in-review.htm /ston/getinvolved/2021-year-in-review.htm
-shot-scraper html https://www.nps.gov/ston/getinvolved/index.htm /ston/getinvolved/index.htm
-shot-scraper html https://www.nps.gov/ston/getinvolved/partners.htm /ston/getinvolved/partners.htm
-shot-scraper html https://www.nps.gov/ston/getinvolved/planprocess.htm /ston/getinvolved/planprocess.htm
-shot-scraper html https://www.nps.gov/ston/getinvolved/volunteer.htm /ston/getinvolved/volunteer.htm
-shot-scraper html https://www.nps.gov/ston/learn/bookstore.htm /ston/learn/bookstore.htm
-shot-scraper html https://www.nps.gov/ston/learn/education-teachers.htm /ston/learn/education-teachers.htm
-shot-scraper html https://www.nps.gov/ston/learn/education/ /ston/learn/education
-shot-scraper html https://www.nps.gov/ston/learn/education/index.htm /ston/learn/education/index.htm
-shot-scraper html https://www.nps.gov/ston/learn/education/loader.cfm /ston/learn/education/loader.cfm
-shot-scraper html https://www.nps.gov/ston/learn/education/new-index.htm /ston/learn/education/new-index.htm
-shot-scraper html https://www.nps.gov/ston/learn/educator-resources.htm /ston/learn/educator-resources.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture.htm /ston/learn/historyculture.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/gaa.htm /ston/learn/historyculture/gaa.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/julius.htm /ston/learn/historyculture/julius.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/lionshead.htm /ston/learn/historyculture/lionshead.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/loader.cfm /ston/learn/historyculture/loader.cfm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/macdougalstreet.htm /ston/learn/historyculture/macdougalstreet.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/oscarwilde.htm /ston/learn/historyculture/oscarwilde.htm
-shot-scraper html https://www.nps.gov/ston/learn/historyculture/portobello.htm /ston/learn/historyculture/portobello.htm
-shot-scraper html https://www.nps.gov/ston/learn/index.htm /ston/learn/index.htm
-shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/activities-for-kids.htm /ston/learn/kidsyouth/activities-for-kids.htm
-shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/index.htm /ston/learn/kidsyouth/index.htm
-shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/junior-ranger.htm /ston/learn/kidsyouth/junior-ranger.htm
-shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/loader.cfm /ston/learn/kidsyouth/loader.cfm
-shot-scraper html https://www.nps.gov/ston/learn/lawsandpolicies.htm /ston/learn/lawsandpolicies.htm
-shot-scraper html https://www.nps.gov/ston/learn/management.htm /ston/learn/management.htm
-shot-scraper html https://www.nps.gov/ston/learn/news.htm /ston/learn/news.htm
-shot-scraper html https://www.nps.gov/STON/learn/news/index.htm /STON/learn/news/index.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/index.htm /ston/learn/photosmultimedia/index.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/interpretative-flags.htm /ston/learn/photosmultimedia/interpretative-flags.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/loader.cfm /ston/learn/photosmultimedia/loader.cfm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/photogallery.htm /ston/learn/photosmultimedia/photogallery.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/ranger-programs-and-special-events.htm /ston/learn/photosmultimedia/ranger-programs-and-special-events.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/stonewall-celebrates-its-5th-anniversary.htm /ston/learn/photosmultimedia/stonewall-celebrates-its-5th-anniversary.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/virtual-fence-exhibit.htm /ston/learn/photosmultimedia/virtual-fence-exhibit.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/virtual-ranger-programs.htm /ston/learn/photosmultimedia/virtual-ranger-programs.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/we-are-stonewall-arts-festival.htm /ston/learn/photosmultimedia/we-are-stonewall-arts-festival.htm
-shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/we-are-stonewall-arts-festival.htm?fbclid=IwAR33CGLnYWvhnuWjJzwfrAs0AaGSyrGV9mYMOK1MII0BiG1QPec6nUNKrFc /ston/learn/photosmultimedia/we-are-stonewall-arts-festival.htm
-shot-scraper html https://www.nps.gov/ston/loader.cfm /ston/loader.cfm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/ /ston/planyourvisit
-shot-scraper html https://www.nps.gov/ston/planyourvisit/accessibility.htm /ston/planyourvisit/accessibility.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/basicinfo.htm /ston/planyourvisit/basicinfo.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/directions.htm /ston/planyourvisit/directions.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/education-programs.htm /ston/planyourvisit/education-programs.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/fees.htm /ston/planyourvisit/fees.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/guidedtours.htm /ston/planyourvisit/guidedtours.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/hours.htm /ston/planyourvisit/hours.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/index.htm /ston/planyourvisit/index.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/passportprogram.htm /ston/planyourvisit/passportprogram.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/permits.htm /ston/planyourvisit/permits.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/permitsandreservations.htm /ston/planyourvisit/permitsandreservations.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/safety.htm /ston/planyourvisit/safety.htm
-shot-scraper html https://www.nps.gov/ston/planyourvisit/things2do.htm /ston/planyourvisit/things2do.htm
-shot-scraper html https://www.nps.gov/ston/siteindex.htm /ston/siteindex.htm
-shot-scraper html https://www.nps.gov/ston/stonewall-celebrates-its-5th-anniversary.htm /ston/stonewall-celebrates-its-5th-anniver
+shot-scraper html https://www.nps.gov/ston/assemblymessage6-5-20.htm -o html/assemblymessage6-5-20.htm
+shot-scraper html https://www.nps.gov/ston/contacts.htm -o html/contacts.htm
+shot-scraper html https://www.nps.gov/ston/faqs.htm -o html/faqs.htm
+shot-scraper html https://www.nps.gov/ston/foundation-document.htm -o html/foundation-document.htm
+shot-scraper html https://www.nps.gov/ston/getinvolved/2016.htm -o html/getinvolved/2016.htm
+shot-scraper html https://www.nps.gov/ston/getinvolved/2021-year-in-review.htm -o html/getinvolved/2021-year-in-review.htm
+shot-scraper html https://www.nps.gov/ston/getinvolved/index.htm -o html/getinvolved/index.htm
+shot-scraper html https://www.nps.gov/ston/getinvolved/partners.htm -o html/getinvolved/partners.htm
+shot-scraper html https://www.nps.gov/ston/getinvolved/planprocess.htm -o html/getinvolved/planprocess.htm
+shot-scraper html https://www.nps.gov/ston/getinvolved/volunteer.htm -o html/getinvolved/volunteer.htm
+shot-scraper html https://www.nps.gov/ston/learn/bookstore.htm -o html/learn/bookstore.htm
+shot-scraper html https://www.nps.gov/ston/learn/education-teachers.htm -o html/learn/education-teachers.htm
+shot-scraper html https://www.nps.gov/ston/learn/education/ -o html/learn/education
+shot-scraper html https://www.nps.gov/ston/learn/education/index.htm -o html/learn/education/index.htm
+shot-scraper html https://www.nps.gov/ston/learn/education/loader.cfm -o html/learn/education/loader.cfm
+shot-scraper html https://www.nps.gov/ston/learn/education/new-index.htm -o html/learn/education/new-index.htm
+shot-scraper html https://www.nps.gov/ston/learn/educator-resources.htm -o html/learn/educator-resources.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture.htm -o html/learn/historyculture.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/gaa.htm -o html/learn/historyculture/gaa.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/julius.htm -o html/learn/historyculture/julius.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/lionshead.htm -o html/learn/historyculture/lionshead.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/loader.cfm -o html/learn/historyculture/loader.cfm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/macdougalstreet.htm -o html/learn/historyculture/macdougalstreet.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/oscarwilde.htm -o html/learn/historyculture/oscarwilde.htm
+shot-scraper html https://www.nps.gov/ston/learn/historyculture/portobello.htm -o html/learn/historyculture/portobello.htm
+shot-scraper html https://www.nps.gov/ston/learn/index.htm -o html/learn/index.htm
+shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/activities-for-kids.htm -o html/learn/kidsyouth/activities-for-kids.htm
+shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/index.htm -o html/learn/kidsyouth/index.htm
+shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/junior-ranger.htm -o html/learn/kidsyouth/junior-ranger.htm
+shot-scraper html https://www.nps.gov/ston/learn/kidsyouth/loader.cfm -o html/learn/kidsyouth/loader.cfm
+shot-scraper html https://www.nps.gov/ston/learn/lawsandpolicies.htm -o html/learn/lawsandpolicies.htm
+shot-scraper html https://www.nps.gov/ston/learn/management.htm -o html/learn/management.htm
+shot-scraper html https://www.nps.gov/ston/learn/news.htm -o html/learn/news.htm
+shot-scraper html https://www.nps.gov/STON/learn/news/index.htm -o html/learn/news/index.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/index.htm -o html/learn/photosmultimedia/index.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/interpretative-flags.htm -o html/learn/photosmultimedia/interpretative-flags.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/loader.cfm -o html/learn/photosmultimedia/loader.cfm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/photogallery.htm -o html/learn/photosmultimedia/photogallery.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/ranger-programs-and-special-events.htm -o html/learn/photosmultimedia/ranger-programs-and-special-events.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/stonewall-celebrates-its-5th-anniversary.htm -o html/learn/photosmultimedia/stonewall-celebrates-its-5th-anniversary.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/virtual-fence-exhibit.htm -o html/learn/photosmultimedia/virtual-fence-exhibit.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/virtual-ranger-programs.htm -o html/learn/photosmultimedia/virtual-ranger-programs.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/we-are-stonewall-arts-festival.htm -o html/learn/photosmultimedia/we-are-stonewall-arts-festival.htm
+shot-scraper html https://www.nps.gov/ston/learn/photosmultimedia/we-are-stonewall-arts-festival.htm?fbclid=IwAR33CGLnYWvhnuWjJzwfrAs0AaGSyrGV9mYMOK1MII0BiG1QPec6nUNKrFc -o html/learn/photosmultimedia/we-are-stonewall-arts-festival.htm
+shot-scraper html https://www.nps.gov/ston/loader.cfm -o html/loader.cfm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/ -o html/planyourvisit
+shot-scraper html https://www.nps.gov/ston/planyourvisit/accessibility.htm -o html/planyourvisit/accessibility.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/basicinfo.htm -o html/planyourvisit/basicinfo.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/directions.htm -o html/planyourvisit/directions.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/education-programs.htm -o html/planyourvisit/education-programs.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/fees.htm -o html/planyourvisit/fees.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/guidedtours.htm -o html/planyourvisit/guidedtours.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/hours.htm -o html/planyourvisit/hours.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/index.htm -o html/planyourvisit/index.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/passportprogram.htm -o html/planyourvisit/passportprogram.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/permits.htm -o html/planyourvisit/permits.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/permitsandreservations.htm -o html/planyourvisit/permitsandreservations.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/safety.htm -o html/planyourvisit/safety.htm
+shot-scraper html https://www.nps.gov/ston/planyourvisit/things2do.htm -o html/planyourvisit/things2do.htm
+shot-scraper html https://www.nps.gov/ston/siteindex.htm -o html/siteindex.htm
+shot-scraper html https://www.nps.gov/ston/stonewall-celebrates-its-5th-anniversary.htm -o html/stonewall-celebrates-its-5th-anniver
